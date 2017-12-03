baad17ea92235c76798ad9c2dc6086e01a1efc43
Michael Odumosu <michaelodumosu57@gmail.com>
2017-12-02 23:01:04 -0500

    Undone work 12/2/17 jquery_rolodex 0.0.2 worked on rolodex, got it to move counterclockwise one step

--- jquery_rolodex/jquery_rolodex.js
+++ jquery_rolodex/jquery_rolodex.js
@@ -1,4 +1,4 @@
-    
+//worked on rolodex, got it to move counterclockwise one step
     
     jQuery.fn.extend({
         rolodex:function(michael,$left,$right){
@@ -6,7 +6,9 @@
             
             var rolodex_set = $(this).length; //this variable gives the size of the rolodex to let the function know what it has to do
             var rolodex_array = []; //to see what items the function is working with
+            var z_array = []; //this variable sees what is facing to the front
             var rolodex_item = 0; //refers to the items in the rolodex
+            var its_ok = 0; //debugger
 
             
             while(rolodex_item != rolodex_set){
@@ -20,26 +22,46 @@
                 }
                 var prev_offset = [$(this).eq(rolodex_prev_item).offset(),"z-index "  +$(this).eq(rolodex_prev_item).css("z-index")];
                 var next_offset = [$(this).eq(rolodex_next_item).offset(),"z-index "  + $(this).eq(rolodex_next_item).css("z-index")];
-                rolodex_array.push([$(this).eq(rolodex_item).clone(), ["object  "  + ( rolodex_prev_item).toString() +"prev is" , prev_offset],["object  " + (rolodex_next_item).toString() +"next is" , next_offset] ]);
+                rolodex_array.push([$(this).eq(rolodex_item), ["object  "  + ( rolodex_prev_item).toString() +"prev is" , prev_offset],["object  " + (rolodex_next_item).toString() +"next is" , next_offset] ]);
+                z_array.push([rolodex_item,parseInt($(this).eq(rolodex_item).css("z-index"))])
                 rolodex_item += 1;
             }
             //this finds out about the positions of every item, it is the control room loop, when the rolodex moves, the rolodex gets updated realtime with this
             
-            
+            function find_face() {
+                //this function goes through the information arrays and finds out which items is facing to the front at the given moment but we might not need to know this
+            }
             
             $left.click(function () {
-                
+                rolodex_array.forEach(function(element){
+                    consoles("counterwise",element);
+                    element[0].offset({top:element[1][1][0].top,left:element[1][1][0].left})
+                    
+                    
+                });
             });
             
             // console logs
-            function consoles(action) {
-                console.log("michael element\n",michael);
-                console.log("clockwise control element\n",$left);
-                console.log("counter-clockwise control element\n",$right);
-                console.log("holding element",$(".rolodex"));
-                console.log( "rolodex_set",rolodex_set);
-                console.log( "rolodex_array \n" ,rolodex_array);
-                console.log("rolodex_item \n" , rolodex_item);
+            function consoles(action,data = undefined,debug = its_ok) {
+                if(debug != 0){
+                    return;
+                }
+                if(action == "counterwise"){
+                    console.log("counterwise seperate actions");
+                    console.log(data)
+                    console.log("where items is",data[0].offset());
+                    console.log("where items must go",data[1][1][0]);
+                }
+                else {
+                    console.log("michael element\n",michael);
+                    console.log("clockwise control element\n",$left);
+                    console.log("counter-clockwise control element\n",$right);
+                    console.log("holding element",$(".rolodex"));
+                    console.log( "rolodex_set",rolodex_set);
+                    console.log( "rolodex_array \n" ,rolodex_array);
+                    console.log( "z_array \n" ,z_array);
+                    console.log("rolodex_item \n" , rolodex_item);
+                }
             }
             //cant use console as function call
 
