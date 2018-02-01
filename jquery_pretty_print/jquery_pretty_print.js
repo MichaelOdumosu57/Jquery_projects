5f1285db368b783dcc8bae1394dd21b9ca6f4a33
Michael Odumosu <michaelodumosu57@gmail.com>
2018-02-01 17:11:55 -0500

    jquery_pretty_print-0.0.3.js this version can indent, set a leftover, cannot center but is able to react when the objects start to leave the container and put them on the next line

--- jquery_pretty_print/jquery_pretty_print.js
+++ jquery_pretty_print/jquery_pretty_print.js
@@ -1,10 +1,12 @@
 
-//this version can vertical reposition proper alginment of objects, place all in one proper row
+//this version can indent, set a leftover, cannot center but is able to react when the objects start to leave the container and put them on the next line
     //capabilities : core pretty print concept in horizontal spacing
     //             : vertical reposition proper alginment
+    //              :indentation, or pretty_print usuable space object
+    //              :recognintion of overstepping horizontal bounds
 
     
-    //planned work : detection of objects
+    //planned work :
     //             : full object pretty print
     
 
@@ -17,10 +19,13 @@
     //              : amimatable detection, if not proper steps would be made to enable it without interfering in front end design
     //              : return error if obejcts cant be pretty printed inside
     
+    
     // properties
     
     //objects -- contains all the obejcts desired to be pretty printed, in the order of input, we could build customization but that is a waste of time and space, if michael is not an object this can be the only argumen
     // spacing -- amount of space between each object in pretty print, this is needed as row creation of how many objects are in the row depends on it, if you don't place one its default is 20
+    // leftover -- decides how much leftover there will be for the last item in the row and the end width of the container object
+    // indent -- decides initial space for row starting, now it 4% because all containers are different
     
 
 var call = 0;
@@ -50,6 +55,10 @@ var call = 0;
                 //////////////////////////////////////////////////////////////
                 //this function takes a dimension and cuts it off and returns the number for use
                 //////////////////////////////////////////////////////////////
+                
+            var pretty_container_width = numberParse($(this).css("width"));
+            var pretty_container_height = numberParse($(this).css("height"));
+            $("body").after("<div class = 'pretty'></div>")
             if(michael.objects === undefined){
                 michael.objects = michael;
                 
@@ -58,31 +67,53 @@ var call = 0;
                 michael.spacing = 20;
             }
             else{
-                $("body").after("<div class = 'pretty'></div>")
                 $(".pretty").css("width",michael.spacing)
                 michael.spacing = numberParse($(".pretty").css("width"))
-                $(".pretty").remove()
             }
+            if(michael.leftover === undefined){
+                michael.leftover = 50;
+            }
+            else{
+                $(".pretty").css("width",michael.leftover)
+                michael.leftover = numberParse($(".pretty").css("width"))
+            }
+            if(michael.indent === undefined){
+                michael.indent = .04 * pretty_container_width;
+            }
+            else{
+                $(".pretty").css("width",michael.indent)
+                michael.indent = numberParse($(".pretty").css("width"))
+            }
+            $(".pretty").remove()
 }
             //////////////////////////////////////////////////////////////
             //  if user wants a quick pretty print they can just put in only what they want pretty printed
+            // any thing with var pretty_container contains information about the the container objects will be pretty printed in
             //////////////////////////////////////////////////////////////
             
             // setting and arraging objects
 {
+            
             var space = michael.spacing;
             var pretty_print_height;
-            var pretty_print_start = {"x":0,"y":0};
+            var pretty_print_start = {"x":michael.indent,"y":0};
+            var pretty_caught = 0;
+            var pretty_leftover = michael.leftover
             var pretty_print = $.map(michael.objects,function (pretty_object,index) {
                 
                 pretty_print_height = numberParse($(pretty_object).css("height"));
+                if( pretty_print_start["x"] > pretty_container_width - pretty_leftover){
+                    console.log("Greater than!!")
+                    pretty_print_start["x"] = michael.indent;
+                    pretty_caught += 1;
+                }
                 $(pretty_object).css({
                     "position":"relative",
                     "left":pretty_print_start["x"],
-                    "top":pretty_print_start["y"] - (pretty_print_height * index)
+                    "top":pretty_print_start["y"] - (pretty_print_height * index) + (pretty_print_height * pretty_caught)
                 })
-                // console.log($(pretty_object).css("left"), pretty_print_start["x"] ,space)
-                console.log($(pretty_object).css("top"), pretty_print_start["y"] ,pretty_print_height)
+                console.log($(pretty_object).css("left"), pretty_print_start["x"] ,space, pretty_container_width - pretty_leftover)
+                // console.log($(pretty_object).css("top"), pretty_print_start["y"] ,pretty_print_height)
                 pretty_print_start["x"] += numberParse($(pretty_object).css("width")) + space
                 return $(pretty_object)
             })
@@ -96,6 +127,7 @@ var call = 0;
             // var space desired space between objects
             // all objects must be have a position:relative attribute to be position properly inside the object
             // var pretty_print_height will contain the height of each object, in the future this variable will contain the height of the object with max height
+            // var pretty_caught is the stablizier, it removes the bug of printing on same row by adding the height of the pretty_object back to the y axis
             //////////////////////////////////////////////////////////////
         }
         
