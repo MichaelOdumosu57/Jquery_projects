9ab93fb98b713d2dfb7a07243a4c4723c76ec5a9
Michael Odumosu <michaelodumosu57@gmail.com>
2018-03-10 09:40:54 -0500

    jquery_adv_clone-0.3.2.js in this version if adv_clone is called more than one times, it know to cut off the first copy as it had made too many clones

--- jquery_adv_clone/jquery_adv_clone.js
+++ jquery_adv_clone/jquery_adv_clone.js
@@ -1,12 +1,11 @@
 
-// in this version developer has capability of adding desired UI method functionalities to each object chosed seperately
+// in this version if adv_clone is called more than one times, it know to cut off the first copy as it had made too many clones
     //capabilities:core cloning functionality
     //            : cloning as many items as needed
     //            : returns a length of all the clones
     //            : responds for single, multiple and mixed calls of any length
     //            : return an array of all the desired clones
     //            : seperation of clones into certain groups
-    //
 
     
     //planned work
@@ -134,6 +133,7 @@ var call = 0;
                 if(call == 1 ){
                     $(this.selector + ":first").remove()
                     console.log(call)
+                    call = 0;
                 }
                 return $.map($(this.selector + (":not(.delete)")),function(value,index){
                     if(michael.specifiers != undefined){
@@ -150,7 +150,7 @@ var call = 0;
                         
                     }
                     
-                    // return $(value).michael.UI();
+                    return $(value);
                     
 
                         
