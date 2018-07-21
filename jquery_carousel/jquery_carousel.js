7b640a3bbfc72512bf4d949f5d8665be9c0fae36
Michael Odumosu <michaelodumosu57@gmail.com>
2018-07-21 16:23:20 -0400

    jquery-carousel-0.2.2.js remove reliances or other external js libaray or imported them into file

--- jquery_carousel/jquery_carousel.js
+++ jquery_carousel/jquery_carousel.js
@@ -1,11 +1,12 @@
-//this function wraps all carousel items into one carousel div to keep it seperate from the rest of the page and enable it to display on smallaer scales
+//remove reliances or other external js libaray or imported them into file
     //capabilities:full carousel functionality
     //            :dual options
 
-    
+
     //planned work : putting more than one item into carousel
     //             : you must avoid using carousel in any variables, functions, obejcts or arguments as this could ruin the extensions functionalty, especially             using carousel choice as a class in a div, im working on this
-    
+    //              :remove all external .js files
+
     // mandatory argument properites
     //  height, height of carousel container
     //  width, width of carousel container
@@ -14,38 +15,80 @@
     //  left, the item that controls the left moving functionality,must be a string that jquery can select the element with
     //  right, the item that controls the right moving functionality,must be a string that jquery can select the element with
     //  rate, rate at which the carousel moves
-    
-    
 
 
-    
+
+
+
 
 var call = 0;
 var call_amounts = [];
+var dimension;
 
 
     jQuery.fn.extend({
         carousel:function(michael){
             // bootstrap has a carousel function but you need the bootstrap.js file to use it which can cause problems, this takes a selector of supposed associated objects for the carousel  along with left and right event control selectors to have the carousel animation
-            
+
             //argument check and initization
+
+    {
+        //helper function
+        function numberParse(dimension){
+            dimension = parseFloat(dimension.split("p")[0])
+            return dimension;
+        }
+        ///////////////////////////////////////////////////////////////////////////////////
+        // for returning the number of an dimenision not in number format only px
+        ///////////////////////////////////////////////////////////////////////////////////
+
+
+        // finds location of important items using this function
+        function BrowserCheck()
+        {
+            var N= navigator.appName, ua= navigator.userAgent, tem;
+            var M= ua.match(/(opera|chrome|safari|firefox|msie|trident)\/?\s*(\.?\d+(\.\d+)*)/i);
+            if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) {M[2]=tem[1];}
+            M= M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+            return M;
+        }
+
+        var browser = [BrowserCheck()];
+        //this will be an array, containing difference among browsers and their versions
+
+
+        var browser_window = window;
+        if(browser[0][0] == "Firefox" && parseFloat(browser[0][1]) >= 57 ){
+          browser_window = window.top;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+        // finding where screen dimensions are
+        ///////////////////////////////////////////////////////////////////////////////////
+
+
+    }
             if (isNaN(michael.rate)){
-                michael.rate = 1000;
+                michael.rate = 750;
+            }
+            if(isNaN(michael.height) || isNaN(michael.width)){
+                console.error("need dimension of how large you want you carousel to be add a width and height property with numbers only")
+                return;
             }
             var refresh = 1;
             console.log(michael.width,michael.height)
             $(michael.left).addClass("carousel_option")
             $(michael.right).addClass("carousel_option")
             $("body").append("<div class = 'carousel_choice'></div> ");
-            
+
 
             ///////////////////////////////////////////////////////////////////////////////////
             // making the moving algorithm much simpler and concise
             ///////////////////////////////////////////////////////////////////////////////////
-            
-            
-            
-            
+
+
+
+
             //the posiitioning process
 {
             var carousel_item_width;
@@ -55,35 +98,37 @@ var call_amounts = [];
             var carousel_item_top_position;
             var carousel_spacing;
             var carousel_item_max_left;
-            
-            
+
+
             var carousel_list = $.map($(this), function(value, index) {
-                
+
                 carousel_item_width = numberParse($(value).css("width"));
                 carousel_item_left = numberParse($(value).css("left"));
                 carousel_item_height = numberParse($(value).css("height"));
                 carousel_spacing = browser_window.outerWidth - carousel_item_left;
                 carousel_item_left_position = $(value).offset().left
-                
-                
+
+
                 $(value).css({
                     "left":carousel_spacing * index,
                     "top" : -(carousel_item_height * index),
                     "margin-bottom":0
                     }).addClass("carousel_item").appendTo($("div.carousel_choice"));
-                        
+
                  carousel_item_top = numberParse($(value).css("top"));
-                
+
                 return [[$(value),carousel_item_left_position,carousel_item_top]];
 
             });
             $("div.carousel_choice").css({
                 height:michael.height,
                 width:michael.width,
+                "position":"relative",
                 overflow:"hidden"
+
             })
             carousel_item_max_left = numberParse($($(this).selector + ":last").css("left"));
-            
+
             console.log(carousel_list,browser_window.outerWidth,carousel_item_max_left)
 }
             ///////////////////////////////////////////////////////////////////////////////////
@@ -93,7 +138,7 @@ var call_amounts = [];
             // make the margin-bottom equal to zero so carousel looks correct
             // carousel_item_max_left is for repositioning to bring the next to the front when called to the right
             ///////////////////////////////////////////////////////////////////////////////////
-            
+
             //the moving process
 {
             var carousel_selected;
@@ -104,19 +149,19 @@ var call_amounts = [];
                 $(".carousel_choice").trigger("option_click");
                 $(this).removeClass("carousel_choice");
                 $(this).show(3 *michael.rate );
-                
-                
-                
+
+
+
 
             })
 
-            
-            
-            
+
+
+
             var carousel_info;
-            
+
             $(".carousel_choice").on("option_click",function() {
-                
+
     {
         {
             //helper function
@@ -127,23 +172,23 @@ var call_amounts = [];
                 '-=': function(a, b) {
                     return a - b;
                 }
-                
+
             };
 
         }
-        
+
         {
         //helper function
         var carousel_repositioner = {
                 '-=': function() {
-                    
+
                     return carousel_item_next_position.forEach(function(position,index){
                     if(position < 0 ){
                         carousel_list[index][0].animate({
                             "left":Math.max.apply(null, carousel_item_prev_position)
-                            
+
                         },refresh)
-                        
+
                     }
                 })
                 },
@@ -152,13 +197,13 @@ var call_amounts = [];
                     if(position > Math.max.apply(null, carousel_item_prev_position) ){
                         carousel_list[index][0].animate({
                             "left":0
-                            
+
                         },michael.rate)
-                        
+
                     }
                 })
                 }
-                
+
             };
 
         }
@@ -171,8 +216,8 @@ var call_amounts = [];
                     console.log("RIGHT");
                     carousel_info = "+=";
                 }
-                
-                
+
+
 
     }
                 var carousel_item_next_position = []
@@ -180,7 +225,7 @@ var call_amounts = [];
 
                 carousel_list.forEach(function(value){
                     if (carousel_info == "+="){
-                        
+
                        if(numberParse(value[0].css("left")) >= carousel_item_max_left ){
                            console.log("hit")
                            value[0].stop().animate({
@@ -188,47 +233,44 @@ var call_amounts = [];
                            },refresh);
                        }
                     }
-                    
+
                 });
 
                 carousel_list.forEach(function(value){
-                    
-    
+
+
                    value[0].animate({
                        left:carousel_info + browser_window.outerWidth.toString()
                    },michael.rate);
                    carousel_item_prev_position.push(numberParse(value[0].css("left")));
-                   
+
                    carousel_item_next_position.push(carousel_operators[carousel_info](numberParse(value[0].css("left")),browser_window.outerWidth));
-    
-    
-                   
-                   
+
+
+
+
                 });
-                
+
                 console.log("carousel_item_prev_position");
                 console.log(carousel_item_prev_position);
                 console.log("carousel_item_next_position");
                 console.log(carousel_item_next_position);
-                
+
                 carousel_repositioner[carousel_info]();
-            
+
             });
 
-            
-            
-            
+
+
+
 }
-            
+
             ///////////////////////////////////////////////////////////////////////////////////
             //  needed to add a div with the carousel choice class becuase, if its never on the page, jquery will ignore it
             // carousel_selected is to let the function extenison know which was clicked and what to do based on that conclusion
             ///////////////////////////////////////////////////////////////////////////////////
 
         }
-        
-        
+
+
     });
-    
-
-
